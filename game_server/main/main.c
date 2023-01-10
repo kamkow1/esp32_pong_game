5c301ed356fd1dae607877801cdd133ab1f2a1a6
kamkow1 <ikamileq@gmail.com>
2023-01-10 21:34:20 +0100

    [server] init

---
+++ game_server/main/main.c
@@ -0,0 +1,20 @@
+/*
+ * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+#include <stdio.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "driver/gpio.h"
+
+#define BLINK_GPIO 2
+
+void app_main(void)
+{
+    printf("siema\n");
+}
