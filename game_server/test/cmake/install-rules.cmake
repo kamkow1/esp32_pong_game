fb529aa24d1d05ffbe44e279aa2099b3bb53bb6a
kamkow1 <ikamileq@gmail.com>
2023-01-25 23:28:58 +0100

    [server] remove test/

--- game_server/test/cmake/install-rules.cmake
+++
@@ -1,8 +0,0 @@
-install(
-    TARGETS game_server_test_exe
-    RUNTIME COMPONENT game_server_test_Runtime
-)
-
-if(PROJECT_IS_TOP_LEVEL)
-  include(CPack)
-endif()
--- game_server/test/source/client_send.h
+++
@@ -1,8 +0,0 @@
-#ifndef CLIENT_SEND_H
-#define CLIENT_SEND_H
-
-#include "lib.h"
-
-void client_send_test(TestResult *test_result);
-
-#endif
