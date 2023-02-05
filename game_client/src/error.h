f97c1b07776a5f280ceb7d621f78612806c11565
kamkow1 <ikamileq@gmail.com>
2023-02-05 22:02:02 +0100

    [client] create a simple SDL2 window and handle keyboard input

---
+++ game_client/src/error.h
@@ -0,0 +1,10 @@
+#ifndef ERROR_H
+#define ERROR_H
+
+#define EXIT_ON_ERROR(cond, args...) if ((cond))  \
+{                                                 \
+  fprintf(stderr, args);                          \
+  exit(1);                                        \
+}                                                 \
+
+#endif
--- game_client/src/main.c
+++ game_client/src/main.c
@@ -1,8 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+
+#include "error.h"
+
+#define WINDOW_WIDTH  800
+#define WINDOW_HEIGHT 600
+#define WINDOW_TITLE  "esp32 pong game client"
+
 
 int main()
 {
-	printf("hello world\n");
-  
-	return 0;
+  EXIT_ON_ERROR(SDL_Init(SDL_INIT_VIDEO) < 0, "failed to initialize SDL2: %s\n", SDL_GetError());
+
+  SDL_Window *window = SDL_CreateWindow(WINDOW_TITLE, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
+  EXIT_ON_ERROR(!window, "failed to crate the game window: %s\n", SDL_GetError());
+
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+  EXIT_ON_ERROR(!renderer, "failed to crate the game renderer: %s\n", SDL_GetError());
+
+  SDL_Event event;
+  bool quit = false;
+  while (!quit) {
+    while (SDL_PollEvent(&event)) {
+      switch (event.type) {
+        case SDL_QUIT: {
+          quit = true;
+          break;
+        }
+
+        case SDL_KEYDOWN: {
+          switch (event.key.keysym.sym) {
+            case SDLK_q: {
+              quit = true;
+              break;               
+            }
+          }  
+        }
+      } 
+    }
+
+    SDL_Surface *surface = SDL_GetWindowSurface(window);
+    SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 18, 18, 18));
+    SDL_UpdateWindowSurface(window);
+  }
+
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
+  return 0;
 }
